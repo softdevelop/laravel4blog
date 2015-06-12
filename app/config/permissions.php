542daf9dccbe53f50a9ae0fce812feacb27e3f73
duong <softdevelop.inc@gmail.com>
2015-06-12 22:57:18 +0700

    Update full site

---
+++ app/config/permissions.php
@@ -0,0 +1,32 @@
+<?php
+
+return array(
+
+	'Global' => array(
+		array(
+			'permission' => 'superuser',
+			'label'      => 'Super User',
+		),
+	),
+
+	'Admin' => array(
+		array(
+			'permission' => 'admin',
+			'label'      => 'Admin Rights',
+		),
+	),
+
+	'Manager' => array(
+		array(
+			'permission' => 'manager',
+			'label'      => 'Manage Rights',
+		),
+	),
+
+	'Author' => array(
+		array(
+			'permission' => 'author',
+			'label'      => 'Author Rights',
+		),
+	),
+);
